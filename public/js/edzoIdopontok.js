04c6a306e9661130e75cf0fd62d5c2a56686813e
BGabesz <bogdan.gabesz@gmail.com>
2022-03-07 12:27:12 +0100

    idopontok osztaly modositasa

--- public/js/edzoIdopontok.js
+++ public/js/edzoIdopontok.js
@@ -1,37 +1,37 @@
 class Idopontok{
-    constructor(){
-        this.idopontokTomb=[];
+    constructor(idopontok){
+        /* this.idopontokElem=$(".tablazat"); */
+        this.idopontokTomb=idopontok;
+        this.eltolas=[];
     }
-    megjelenit(idopontok,myCallback){
-        console.log(idopontok);
-        
-        
-
-        let hányOszlopos=7;
-        
+    megjelenit(/* idopontok, */myCallback,eltolas,hanyOszlopos){
+        /* this.eltolas.push(0); */
+        /* console.log(eltolas); */
+        /* let eltolas=1; */
+        /* let hányOszlopos=7; */
         const hetNapjai = ["Vasárnap","Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat"];
-        let tablaSzoveg="<table><thead><tr>";
-        for (let index = 0; index < hányOszlopos; index++) {//tábla cím hány oszlopos
+        let tablaSzoveg=/* gombB+gombJ+ */"<table><thead><tr>";
+        for (let index = 0+eltolas; index < hanyOszlopos+eltolas; index++) {//tábla cím hány oszlopos
             var datum = new Date(myCallback(index));
             tablaSzoveg+='<th>'+hetNapjai[datum.getDay()]+'<br>'+myCallback(index).slice(0,10)+'</th>'
         }
         tablaSzoveg+='</tr></thead>';
         tablaSzoveg+='<tbody>';
         tablaSzoveg+="<tr>"
-        for (let index = 0; index < hányOszlopos; index++) {
+        for (let index = 0; index < hanyOszlopos; index++) {
             tablaSzoveg+="<td class=tablaadat"+index+">";
             tablaSzoveg+="</td>";
-            }
-            tablaSzoveg+="</tr>"
-            tablaSzoveg+='</tbody>';
+        }
+        tablaSzoveg+="</tr>"
+        tablaSzoveg+='</tbody>';
             tablaSzoveg+='</table>';
             $(".tablazat").html(tablaSzoveg);
-            const szuloElem = $('.tablaadat');
+            /* const szuloElem = $('.tablaadat');
             const sablonElem = $('footer .idopont');
             sablonElem.show();
             szuloElem.empty();
             for (let index = 0; index < hányOszlopos; index++) {
-                    idopontok.forEach(function (elem) {
+                this.idopontokTomb.forEach(function (elem) {
                         console.log(index);
                         let datum1=elem.datum.slice(0,10);
                         let datum2=myCallback(index).slice(0,10);
@@ -41,12 +41,26 @@ class Idopontok{
                         }
                     });
             }
-        sablonElem.hide();
-        /* $(window).on('felold', (event) => {
-            console.log("torol"+event.detail.id);
+            sablonElem.hide(); */
+            /* $(window).on('felold', (event) => {
+                console.log("torol"+event.detail.id);
             myAjax.adattorles(apiVegpont, event.detail.id);
             apiVegpont = "http://localhost:4005/szemelyiEdzesek";
+        }); */
+        /* $(".JobbraNovel").on("click", () => {//rákattintás az oldalon
+            this.eltolas++;
+            console.log(eltolas);
+          });
+          $(".BalraCsokkent").on("click", () => {//rákattintás az oldalon
+            this.eltolas--;
+            console.log(eltolas);
           }); */
+    }
+    kattintasTrigger(esemenyneve) {
+        let esemeny = new CustomEvent(esemenyneve, {
+            detail: this.adat,
+        });
+        window.dispatchEvent(esemeny);
     }
     letrehoz(){
 
