97b4c05f9c095fae4533e6d788b540c28063d44e
BGabesz <bogdan.gabesz@gmail.com>
2022-03-17 16:00:08 +0100

    szemely nevre kereses visszaállít

--- app/Http/Controllers/api/SzemelyController.php
+++ app/Http/Controllers/api/SzemelyController.php
@@ -19,6 +19,7 @@ class SzemelyController extends Controller
         $sort=$request->query ('_sort');
         $order=$request->query ('_order');
         $q=$request->query('q');
+        $nev=$request->query('nev');
         $szemelyek=Szemely::selectRaw("*");
         if($sort&&$order){
             $szemelyek->orderBy($sort,$order);
@@ -30,6 +31,8 @@ class SzemelyController extends Controller
                 $szemelyek->orWhere($column,'like','%'.$q.'%');
                 $szemelyek->orWhere($column,$q);
             };
+        }if($nev){
+            $szemelyek->where('nev','like','%'.$nev.'%');
         }
         //$szemelyek= ($sort&&$order) ? Szemely::orderBy($sort,$order)->get(): Szemely::all();
       
