646a476b2f871ba797eeeb6153082db533816d2f
BGabesz <bogdan.gabesz@gmail.com>
2022-03-21 18:29:29 +0100

    UgyfelEdzesSzemellyel osszekapcsolas

--- app/Http/Controllers/api/UgyfelEdzesSzemellyel.php
+++ app/Http/Controllers/api/UgyfelEdzesSzemellyel.php
@@ -18,9 +18,22 @@ class UgyfelEdzesSzemellyel extends Controller
             ->join('szemelies','szemelies.szemely_id','ugyfel_edzes.ugyfel')
             ->select('ugyfel_edzes.id','ugyfel_edzes.edzo','ugyfel_edzes.ugyfel','szemelies.nev','ugyfel_edzes.datum','ugyfel_edzes.ora',)
             ->get();
+            
         $rendez=$tabla->sortBy('datum');
         /* echo"hopp"; */
         $rendez->all();
         return $rendez->values()->all();
+    }
+    public function osszekapcsol()
+    {
+        $tabla= DB::table('szemelies')
+            ->join('berlets','berlets.ugyfel','szemelies.szemely_id')
+            ->join('berlet_tipuses','berlet_tipuses.berlet_tipus_id','berlets.berlet_tipus_id')
+            ->select('szemelies.szemely_id','szemelies.nev','szemelies.kep','szemelies.neme','berlets.datum_tol')
+            ->get();
+         
+        /* echo"hopp"; */
+        $tabla->all();
+        return $tabla->values()->all();
     }
 }
