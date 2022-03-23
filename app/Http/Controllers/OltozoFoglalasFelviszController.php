1048f82f31f8b117ee266f8b2aac30342e2e098a
BGabesz <bogdan.gabesz@gmail.com>
2022-03-23 14:31:41 +0100

    oltozofoglalas controller működik

--- app/Http/Controllers/OltozoFoglalasFelviszController.php
+++ app/Http/Controllers/OltozoFoglalasFelviszController.php
@@ -3,60 +3,84 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Ugyfel_edzes;
+use App\Models\Szemely;
 use App\Models\Oltozofoglalas;
 use App\Models\Berlet;
 use Illuminate\Support\Facades\DB;
 Use \Carbon\Carbon;
 
+
 class OltozoFoglalasFelviszController extends Controller
 {
     public function OltozoFoglalas(Request $request){
-        
         $request->validate([
             'ugyfelNev'=>'required',
             'szekrenySzama'=>'required|integer'
         ]);
         $szemelyNev=$request -> ugyfelNev;
+        $szekrenySzam=$request -> szekrenySzama;
+        $szoveg="";
+        $seged=false;
         $tabla= DB::table('szemelies')
-        ->select('szemely_id')
-        ->where('nev','like','%'.$szemelyNev.'%')
-        ->pluck('szemely_id');
-        $tabla = trim($tabla, '[]');
-        $users = Berlet::select("*",
-                    DB::raw('(CASE 
-                        WHEN users.status = "0" THEN "User" 
-                        WHEN users.status = "1" THEN "Admin" 
-                        ELSE "SuperAdmin" 
-                        END) AS status_lable'))
-                ->get();
-        $tabla= DB::table('berlets')
-            ->select('datumIgaz')
-            ->where('(datum_tol<Now() and Now() <datum_ig )as datumIgaz')
-            ->get();
-        /* $jelenlegiDatum = Carbon::now()->addHour(); */
+            ->select('*')
+            ->Where('nev','like','%'.$szemelyNev.'%')
+            ->first();
+        if ($tabla) {
+            $szoveg.="Talált ilyen embert";
 
-        $ujfoglalas =new Oltozofoglalas;
-        /* $nap=date('Y-m-d H:i:s'); */
-        $ujfoglalas->szekreny_id=$request -> szekrenySzama;
-        $ujfoglalas->ugyfel=$tabla;
-        /* $ujfoglalas->datum=DB::RAW('NOW()'); *///1 órával kevesebbet ad
-        /* $ujfoglalas->datum=$jelenlegiDatum; */
-        $ujfoglalas->datum=Now();//1 órával kevesebbet ad
-        $res = $ujfoglalas -> save();
-        DB::table('szekenies')
-            ->where('szekreny_id', $request -> szekrenySzama)
-            ->update(['ures_e' => 'F']);
+            $tabla2= DB::table('berlets')
+            ->select('*')
+            ->Where('ugyfel','=',$tabla->szemely_id)
+            ->Where('datum_tol','<=',Now())
+            ->Where('datum_ig','>=',Now())
+            ->first();
+            if($tabla2){
+                $szoveg.=", van bérlete";
+
+                $tabla3=DB::table('szekenies')
+                ->select('*')
+                ->Where('szekreny_id','=',$szekrenySzam)
+                ->Where('ures_e','=','Ü')
+                ->first();
+                if($tabla3){
+                    $szoveg.=", üres volt a szekrény";
+                    $seged=true;
+                    //Carbon::now('Europe/Stockholm'))
+                    //$jelenlegiDatum = Carbon::now()->addHour(); 
+                    $ujfoglalas =new Oltozofoglalas;
+                    // $nap=date('Y-m-d H:i:s'); 
+                    $ujfoglalas->szekreny_id=$szekrenySzam;
+                    $ujfoglalas->ugyfel=$tabla->szemely_id;
+                    // $ujfoglalas->datum=DB::RAW('NOW()'); //1 órával kevesebbet ad
+                    // $ujfoglalas->datum=$jelenlegiDatum; 
+                    $ujfoglalas->datum=Now();//1 órával kevesebbet ad
+                    $ujfoglalas->save();
+                    DB::table('szekenies')
+                    ->orWhere('szekreny_id', $szekrenySzam)
+                    ->update(['ures_e' => 'F']);
+                    
+                }else if(!$tabla3){
+                    $szoveg.=",HIBA de nem  volt üres a szekrény";
+                }
+            }else if(!$tabla2){
+                $szoveg.=",HIBA de nincs jelenleg bérlete";
+            }
+         }else if(!$tabla){
+            $szoveg.=" HIBA,Nem talált ilyen embert";
+         }
         
-        if($res===1){
-            return back()->with('sikeres','A foglalás sikeres volt'.$tabla);
-        }else{
-            return back()->with('sikertelen','A foglalás sikertelen volt');
+        
+        if($seged){//ha igaz akkor
+            return back()->with('sikeres','A foglalás sikeres volt '.$szoveg);
+        }else{//ha hamis akkor
+            return back()->with('sikertelen','A foglalás sikertelen volt '.$szoveg);
         }
 
 
     }
-    
+    public function OltozoFeloldas(Request $request){
+        
+    }
     
 
 }
