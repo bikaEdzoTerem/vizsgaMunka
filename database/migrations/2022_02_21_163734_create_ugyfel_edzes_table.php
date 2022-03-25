0446c2bbae3a7cbb6891cbaf045a8005bcb7efac
BGabesz <bogdan.gabesz@gmail.com>
2022-03-25 13:02:09 +0100

    migráció módosítása id hozzáadása

--- database/migrations/2022_02_21_163734_create_ugyfel_edzes_table.php
+++ database/migrations/2022_02_21_163734_create_ugyfel_edzes_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('ugyfel_edzes', function (Blueprint $table) {
+            $table->increments('id'); 
             //külső kulcs
         $table->integer('edzo')->unsigned();		
         $table->foreign('edzo')->references('szemely_id')->on('szemelies');
