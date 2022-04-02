467dcf5cc7317314d6d420b9fc0415d3d5ff4804
BGabesz <bogdan.gabesz@gmail.com>
2022-04-02 16:41:35 +0200

    kis módosítások,szöveg szinezés,stb

--- public/js/szekreny.js
+++ public/js/szekreny.js
@@ -2,26 +2,26 @@ class Szekreny {
   constructor(node,adat)  {
     this.node = node;
     this.adat = adat;
-    this.syekreny_szama = this.node.children(".szama");
+    this.syekreny_szama = this.node.children().children(".szama");
     this.syekreny_szama.text(adat.szekreny_id);
 
-    this.szekreny_neme = this.node.children(".neme");
+    this.szekreny_neme = this.node.children().children(".neme");
     this.szekreny_neme.text(adat.tipusa);
 
-    this.szekreny_uresE = this.node.children(".uresE");
+    this.szekreny_uresE = this.node.children().children(".uresE");
     if(adat.ures_e==="Ü"){
-      this.szekreny_uresE.text("Üres");
+      this.szekreny_uresE.text("Üres").css("color", "green");
     }else if(adat.ures_e==="R"){
-      this.szekreny_uresE.text("Rossz!");
+      this.szekreny_uresE.text("Rossz!").css("color", "rgb(105, 0, 0)");
     }else if(adat.ures_e==="F"){
-      this.szekreny_uresE.text("Foglalt");
+      this.szekreny_uresE.text("Foglalt").css("color", "orange");
     }
-    this.szekreny_feloldas = this.node.children().children(".feloldasGomb");
+    this.szekreny_feloldas = this.node.children().children().children(".feloldasGomb");
     this.szekreny_feloldas.on("click ", () => {
       this.kattintasTrigger("felold");
     });
 
-    this.szekreny_rosszCheckBox = this.node.children().children(".hibasGomb");
+    this.szekreny_rosszCheckBox = this.node.children().children().children(".hibasGomb");
     this.szekreny_rosszCheckBox.on("input ", () => {
       this.kattintasTrigger("rossz");
     });
--- resources/views/pages/recepcio.blade.php
+++ resources/views/pages/recepcio.blade.php
@@ -3,79 +3,77 @@
 <main>
       <header>
         <form action="{{route('OltozoFoglalasFelvitel1')}}" method="post">
+          @csrf
           <div id="kereso">
-            @csrf
-            <div id="lekerekitve"></div>
+            {{-- <div id="lekerekitve"></div> --}}
             <div class="bal1">
               <label id="keresoSzoveg"for="fname" >Személy kereső:</label>
               <input type="txt" id="keresoInput" placeholder="Személy kereső" class="keresSzemely" name="ugyfelNev" value="{{old('ugyfelNev')}}"/>
               <div class="keresettSzemely"></div>
             </div>
-            <div id="lekerekitve"></div>
+            {{-- <div id="lekerekitve"></div> --}}
             <div class="jobb1">
               <label id="keresoSzoveg"for="fname">Szekrény kereső:</label>
               <input type="number" id="keresoInput" placeholder="Szekrény kereső" min="0" class="keresSzekrenykulcs" name="szekrenySzama" value="{{old('szekrenySzama')}}"/>
-                <table class="keresettSzekreny"></table>
+                <div class="keresettSzekreny"></div>
+            </div>
+            <div class="gomb">
+              <button type="submit" class="lefoglal">Lefoglal</button>
             </div>
           </div>
-              <button type="submit" class="lefoglal">Lefoglal</button><br>
               @if(Session::has('sikeres'))
                 <div class="hibaüzenet sikeresUzenet">{{Session::get('sikeres')}}</div>
               @endif
               @if(Session::has('sikertelen'))
                 <div class="hibaüzenet sikertelenUzenet">{{Session::get('sikertelen')}}</div>
               @endif
-          </form>
+        </form>
           <div class="letszam">
             <p id="SzabadSzoveg">Férfi szabad hely:</p>
-            <span id="ferfiLetszam"></span>
+            <span class="ferfiLetszam"></span>
             <p id="SzabadSzoveg">Női szabad hely:</p>
-            <span id="noiiLetszam">100</span>
+            <span class="noiiLetszam">100</span>
           </div>
       </header>
-    
-    <div id="szemelyMegjelenites" class="Lefoglatak">
-    </div>
     <section>
-      <button type="submit" class="osszesFeloldasGomb">Felold</button>
+      <div class="szekrenyValtoztat">
+      <select class="szekrenyekValasztas"></select>
+      <button type="submit" class="osszesFeloldasGomb">Összes felold</button>
+    </div>
       <div  class="szekrenyek"></div>
     </section>
   <footer>
     <div class="szemely">
-      <label id="keresoSzoveg"for="fname">Neve: </label>
-      <p class="neve"></p><br>
-      <label id="keresoSzoveg"for="fname">Neme: </label>
-      <p class="neme"></p><br>
-      <label id="keresoSzoveg"for="fname">Igazolvány száma: </label>
-      <p class="igazolvanySzama"></p><br>
-      <label id="keresoSzoveg"for="fname">Igazolvány típusa: </label>
-      <p class="igazolvanyTipusa"></p><br>
-      <button type="button" class="felviszGomb">Felvisz</button><br>
-      <label id="keresoSzoveg"for="fname">E-mail címe: </label>
-      <p class="email"></p><br>
+      {{-- <label id="keresoSzoveg"for="fname">Neve: </label> --}}
+      <p class="neve"></p>{{-- <br> --}}
+      {{-- <label id="keresoSzoveg"for="fname">Neme: </label> --}}
+      <p class="neme"></p>{{-- <br> --}}
+      {{-- <label id="keresoSzoveg"for="fname">Igazolvány száma: </label> --}}
+      <p class="igazolvanySzama"></p>{{-- <br> --}}
+      {{-- <label id="keresoSzoveg"for="fname">Igazolvány típusa: </label> --}}
+      <p class="igazolvanyTipusa"></p>{{-- <br> --}}
+      <button type="button" class="felviszGomb">Felvisz</button>{{-- <br> --}}
+      {{-- <label id="keresoSzoveg"for="fname">E-mail címe: </label> --}}
+      <p class="email"></p>{{-- <br> --}}
       {{-- <img src="img_girl.jpg" alt="profilkepe" class="szemelyKep"> --}}
-      <label id="keresoSzoveg"for="fname">Bérlete : </label>
+      {{-- <label id="keresoSzoveg"for="fname">Bérlete : </label> --}}
       <p class="berlete"></p>
     </div>
     
   <table >
-    <!-- <tbody>
-    <tr class="szekreny">
-      <td class="szama"></td>
-      <td class="neme"></td>
-      <td class="uresE"></td>
-      <td><button type="submit" class="feloldasGomb">Felold</button></td>
-      <td class=""><input type="checkbox" class="hibasGomb" name="switch" ></td>
-    </tr>
-  </tbody> -->
   <div class="szekreny">
-    <p class="szama"></p><br>
-    <p class="neme"></p><br>
-    <p class="uresE"></p><br>
-    <p ><button type="submit" class="feloldasGomb">Felold</button></p><br>
-    <p ><input type="checkbox" class="hibasGomb" name="switch" ></p><br>
+    <div class="circle">
+      <p class="szama"></p>
+    </div>
+    <div class="tartalom">
+      <p class="neme"></p>
+      <p class="uresE"></p>
+      <p ><button type="submit" class="feloldasGomb">Felold</button></p>{{-- <br> --}}
+      <p class="check"><input type="checkbox" class="hibasGomb" name="switch" ></p>
+    </div>
   </div>
-    © Minden jog fenntartva ©<br /></footer>
+    © Minden jog fenntartva ©<br />
+  </footer>
 </main>
 </body>
 </html>
