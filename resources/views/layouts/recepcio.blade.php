895ba309561504f2bfdb6b0bf186256deb1dfd7b
BGabesz <bogdan.gabesz@gmail.com>
2022-04-07 01:06:01 +0200

    kis szerkesztes,css ,js hozzaadasa

--- resources/views/layouts/recepcio.blade.php
+++ resources/views/layouts/recepcio.blade.php
@@ -3,6 +3,7 @@
         <meta charset="UTF-8" />
         <meta name="description" content="RecepciosOldal" />
         <meta name="author" content="Bogdán Gábor" />
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script src="../js/ajax.js"></script>
         <script src="../js/szekreny.js"></script>
@@ -10,12 +11,13 @@
         <script src="../js/recepcio.js"></script>
         <script src="../js/szemely.js"></script>
         @yield('css')
-        <link href="../css/RecepcioCss/section.css" rel="stylesheet" type="text/css" />
-        <link href="../css/RecepcioCss/alert.css" rel="stylesheet" type="text/css" />
         <link href="../css/RecepcioCss/html.css" rel="stylesheet" type="text/css" />
         <link href="../css/RecepcioCss/header.css" rel="stylesheet" type="text/css" />
+        <link href="../css/RecepcioCss/alert.css" rel="stylesheet" type="text/css" />
+        <link href="../css/RecepcioCss/section.css" rel="stylesheet" type="text/css" />
         <link href="../css/RecepcioCss/szekrenyek.css" rel="stylesheet" type="text/css" />
         <link href="../css/RecepcioCss/kartya.css" rel="stylesheet" type="text/css" />
+        
         <title>Recepcios Felület</title>
     </head>
     <body>
