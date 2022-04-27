c2842fa855c590080a7138bd6f212bfb5517825f
BGabesz <bogdan.gabesz@gmail.com>
2022-04-28 01:57:43 +0200

    email re foglalas

--- app/Http/Controllers/UgyfeledzesFelviszController.php
+++ app/Http/Controllers/UgyfeledzesFelviszController.php
@@ -3,23 +3,21 @@
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Ugyfel_edzes;
-use Illuminate\Support\Facades\DB;
-use App\Http\Controllers\Post;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class UgyfeledzesFelviszController extends Controller{
     public function felviszUgyfelFoglalas (Request $request){/* felviszUgyfelFoglalas */
         
         $request->validate([
             'ugyfelNev'=>'required',
-            'edzo'=>'required|integer',
             'datum'=>'required',
             'orara'=>'required',
             'ora'=>'required'
         ]);
         $szemelyNev=$request -> ugyfelNev;
-        $tabla= DB::table('users')
-        ->select('id')
-        ->where('name','like','%'.$szemelyNev.'%')
+        $tabla=User::select('id')
+        ->where('email','like','%'.$szemelyNev.'%')
         ->pluck('id');
         $tabla = trim($tabla, '[]');
         if(strlen($tabla)>0){
@@ -28,12 +26,12 @@ class UgyfeledzesFelviszController extends Controller{
             $seged=($request -> datum);
             $seged2=($request -> orara);
             $seged3=$seged." ".$seged2;
-            $ujfoglalas->edzo=$request -> edzo;
+            $ujfoglalas->edzo=auth()->user()->id;//vagy Auth::id()
             $ujfoglalas->ugyfel=$tabla;
             $ujfoglalas->datum=$seged3;
             $ujfoglalas->ora=$request -> ora;
             $ujfoglalas -> save();
-            $ellenorzes=DB::table('ugyfel_edzes')->where('ugyfel','=', $tabla)->where('datum','=', $seged3)->where('ora','=', $request -> ora)
+            $ellenorzes=Ugyfel_edzes::where('ugyfel','=', $tabla)->where('datum','=', $seged3)->where('ora','=', $request -> ora)
             ->pluck('ugyfel');
             $tabla = trim($ellenorzes, '[]');;
             if(strlen($tabla)>0){
