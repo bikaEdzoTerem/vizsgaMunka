d275fc2fd513a7be950ee784b011fbe3b8ea446d
BGabesz <bogdan.gabesz@gmail.com>
2022-04-28 02:01:41 +0200

    egysegesites

--- app/Http/Controllers/api/UgyfelEdzesController.php
+++ app/Http/Controllers/api/UgyfelEdzesController.php
@@ -4,26 +4,28 @@ namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Ugyfel_edzes;
-use App\Models\Szemely;
+use App\Models\User;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class UgyfelEdzesController extends Controller{
     public function index(Request $request){//összekapcsolja a users táblával és visszaadja azokat az elemeket amiket a selectbe vannak
         $datumSzemelyel=$request->query('datumTolIgSzemellyel');
         if($datumSzemelyel){
-            $tabla= DB::table('ugyfel_edzes')
-                ->join('users','users.id','ugyfel_edzes.ugyfel')
+            $tabla=Ugyfel_edzes::join('users','users.id','ugyfel_edzes.ugyfel')
                 ->select('ugyfel_edzes.edzo','ugyfel_edzes.ugyfel','users.name','ugyfel_edzes.datum','ugyfel_edzes.ora',)
                 ->Where('datum','>=',substr($datumSzemelyel,0,10))
                 ->Where('datum','<=',substr($datumSzemelyel,11,22))
                 ->Where('name','like','%'.substr($datumSzemelyel,22).'%')
+                /* ->Where('ugyfel_edzes.edzo','=','24') */
+                ->Where('ugyfel_edzes.edzo','=',Auth::id())
                 ->get();
+                /* dd(Auth::id()); */
         }else{
-            $tabla= DB::table('ugyfel_edzes')
-                ->join('users','users.id','ugyfel_edzes.ugyfel')
+            $tabla= Ugyfel_edzes::join('users','users.id','ugyfel_edzes.ugyfel')
                 ->select('ugyfel_edzes.edzo','ugyfel_edzes.ugyfel','users.name','ugyfel_edzes.datum','ugyfel_edzes.ora',)
+                /* ->Where('ugyfel_edzes.edzo','=',auth()->user()->id) */
                 ->get();
         }
         $rendez=$tabla->sortBy('datum');
@@ -32,8 +34,7 @@ class UgyfelEdzesController extends Controller{
     }
     public function delete(Request $request){
         $original=$request->all();
-        DB::Table('ugyfel_edzes')
-       ->where([
+        Ugyfel_edzes::where([
            ['edzo',$original['edzo']],
            ['ugyfel',$original['ugyfel']],
            ['datum',$original['datum']],
@@ -54,13 +55,11 @@ class UgyfelEdzesController extends Controller{
             'edzo'=>'required',
         ]);
         $original=$request->all();
-        $szemely=DB::Table('users')
-            ->select("id")
+        $szemely=User::select("id")
             ->where('name','like','%'.$request -> nevUj.'%')
             ->pluck('id');
         $szemely = trim($szemely, '[]');
-        /* $ugyfelEdzes= */DB::Table('ugyfel_edzes')
-        ->where([
+        Ugyfel_edzes::where([
             ['edzo',$original['edzo']],
             ['ugyfel',$original['ugyfelR']],
             ['datum',$original['datumR']],
